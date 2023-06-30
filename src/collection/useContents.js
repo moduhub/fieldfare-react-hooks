e8b337ae4bd019b027b297f4bb8eac384918ab31
Adan Kvitschal <adan@moduhub.com>
2023-06-30 15:59:53 -0300

    [fix] better collection contents sync

--- src/collection/useContents.js
+++ src/collection/useContents.js
@@ -1,5 +1,6 @@
 import { ChunkList, ChunkSet } from "@fieldfare/core";
-import { useEffect, useReducer, useRef} from "react";
+import { Chunk } from "@fieldfare/core";
+import { useEffect, useRef, useState, useReducer} from "react";
 
 /**
  * Use an up-to-date set built from the Chunks of a collection element, allows transforming
@@ -14,16 +15,18 @@ import { useEffect, useReducer, useRef} from "react";
  * @param {Function|AsyncFunction} filter Function to filter the chunk before it is added to the buffer.
  * @returns state containing the status of the request, the transformed contents and the error if any.
  */
-export function useContents(collection, elementName, transform = (chunk) => chunk, filter= (chunk) => true) {
+export function useContents(collection, elementName, transform = (chunk) => chunk, filter = () => true) {
     const initialState = {
         status: 'loading',
-        chunks: undefined,
+        contents: undefined,
         error: undefined
     };
-    const expandedIdentifiers = useRef();
     const [state, dispatch] = useReducer((prevState, newState) => {
         switch(newState.status) {
             case 'loading': {
+                if(prevState.status === 'loading') {
+                    return prevState;
+                }
                 return {
                     ...prevState,
                     status: 'loading'
@@ -32,14 +35,14 @@ export function useContents(collection, elementName, transform = (chunk) => chun
             case 'loaded': {
                 return {
                     status: 'loaded',
-                    chunks: newState.chunks ? newState.chunks : prevState.chunks,
+                    contents: newState.contents ? newState.contents : prevState.contents,
                     error: undefined
                 };
             }
             case 'error': {
                 return {
                     status: 'error',
-                    chunks: undefined,
+                    contents: undefined,
                     error: newState.error
                 };
             }
@@ -48,89 +51,79 @@ export function useContents(collection, elementName, transform = (chunk) => chun
             }
         }
     }, initialState);
+    const expandedIdentifiers = useRef();
+    // const [status,setStatus] = useState('loading');
+    // const [contents,setContents] = useState(undefined);
+    // const [error,setError] = useState(undefined);
+    const [eventCounter, setEventCounter] = useState(0);
+    const refreshContents = async () => {
+        let newContents;
+        if(!expandedIdentifiers.current) {
+            expandedIdentifiers.current = new Set();
+        }
+        const element = await collection.getElement(elementName);
+        if(!element) {
+            throw Error('NOT_DEFINED');
+        }
+        if(element instanceof ChunkSet === false
+        && element instanceof ChunkList === false) {
+            throw Error('INVALID_TYPE');
+        }
+        for await (const chunk of element) {
+            const chunkIdentifier = chunk.id;
+            if(!expandedIdentifiers.current.has(chunkIdentifier)) {
+                if(!newContents) {
+                    newContents = new Map(state.contents);
+                }
+                const transformed = await transform(chunk);
+                if(!await filter(transformed)) {
+                    continue;
+                }
+                expandedIdentifiers.current.add(chunkIdentifier);
+                newContents.set(chunkIdentifier, transformed);
+            }
+        }
+        for(const chunkIdentifier of expandedIdentifiers.current) {
+            if(!await element.has(Chunk.fromIdentifier(chunkIdentifier))) {
+                if(!newContents) {
+                    newContents = new Map(state.contents);
+                }
+                newContents.delete(chunkIdentifier);
+                expandedIdentifiers.current.delete(chunkIdentifier);
+            }
+        }
+        return newContents;
+    };
     useEffect(() => {
-        if(!collection) {
+        if(!collection
+        || !elementName) {
             return;
         }
-        const loadContents = async (prevContents) => {
-            let newContents;
-            if(!expandedIdentifiers.current) {
-                expandedIdentifiers.current = new Set();
-            }
-            const element = await collection.getElement(elementName);
-            if(!element) {
-                throw Error('NOT_DEFINED');
-            }
-            if(element instanceof ChunkSet === false
-            && element instanceof ChunkList === false) {
-                throw Error('INVALID_TYPE');
-            }
-            for await (const chunk of element) {
-                const chunkIdentifier = chunk.id;
-                if(!expandedIdentifiers.current.has(chunkIdentifier)) {
-                    if(!newContents) {
-                        newContents = new Map(prevContents);
-                    }
-                    const transformed = await transform(chunk);
-                    if(!await filter(transformed)) {
-                        continue;
-                    }
-                    expandedIdentifiers.current.add(chunkIdentifier);
-                    newContents.set(chunkIdentifier, transformed);
-                }
-            }
-            for(const chunkIdentifier of expandedIdentifiers.current) {
-                if(!await element.has(chunkIdentifier)) {
-                    if(!newContents) {
-                        newContents = new Map(prevContents);
-                    }
-                    newContents.delete(chunkIdentifier);
-                    expandedIdentifiers.current.delete(chunkIdentifier);
-                }
-            }
-            return newContents;
-        };
         dispatch({status: 'loading'});
-        loadContents(state.chunks)
+        refreshContents()
         .then(newContents => {
             if(newContents) {
-                dispatch({
-                    status: 'loaded',
-                    chunks: newContents
-                });
+                dispatch({status: 'loaded', contents: newContents});
             } else {
                 dispatch({status: 'loaded'});
             }
         })
         .catch(error => {
-            dispatch({
-                status: 'error',
-                error: error
-            });
+            dispatch({status: 'error', error});
         });
+    }, [collection, elementName, eventCounter]);
+    useEffect(() => {
+        if(!collection
+        || !elementName) {
+            return;
+        }
         const listener = collection.events.on(elementName + '.change', (chunkSet) => {
-            dispatch({status: 'loading'});
-            loadContents(state.chunks)
-            .then(newContents => {
-                if(newContents) {
-                    dispatch({
-                        status: 'loaded',
-                        chunks: newContents
-                    });
-                } else {
-                    dispatch({status: 'loaded'});
-                }
-            })
-            .catch(error => {
-                dispatch({
-                    status: 'error',
-                    error: error
-                });
-            });
+            console.log('useContents onChange', eventCounter);
+            setEventCounter(eventCounter + 1);
         });
         return () => {
             collection.events.removeEventListener(listener);
         };
-    }, [collection, elementName]);
+    }, [collection, elementName, eventCounter]);
     return state;
 }
