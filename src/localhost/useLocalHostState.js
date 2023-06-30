ef8f80d4d5bdd8e3e795a3c0f9afda00e89fc69f
Adan Kvitschal <adan@moduhub.com>
2023-06-30 16:01:35 -0300

    [fix] better state keeping and update check

--- src/localhost/useLocalHostState.js
+++ src/localhost/useLocalHostState.js
@@ -1,35 +1,38 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { LocalHost, Collection } from '@fieldfare/core';
 
 export function useLocalHostState() {
     const [announceMessage, setAnnounceMessage] = useState(undefined);
-    const [collectionStates, setCollectionStates] = useState(undefined);
+    const collectionStates = useRef(undefined);
     useEffect(() => {
         const checkInterval = setInterval(() => {
             Collection.getLocalCollectionsStates().then((newCollectionStates) => {
-                if(!collectionStates) {
-                    setCollectionStates(newCollectionStates);
-                    return;
-                }
-                for(const prop in newCollectionStates) {
-                    if(!collectionStates[prop]
-                    || collectionStates[prop] !== newCollectionStates[prop]) {
-                        console.log('collection states updated ' + prop + ' from ' + collectionStates[prop] + ' to ' + newCollectionStates[prop]);
-                        setCollectionStates(newCollectionStates);
-                        return;
+                let changed = false;
+                if(collectionStates.current) {
+                    for(const prop in newCollectionStates) {
+                        if(!collectionStates.current[prop]
+                        || collectionStates.current[prop] !== newCollectionStates[prop]) {
+                            console.log('collection states updated ' + prop + ' from ' + collectionStates[prop] + ' to ' + newCollectionStates[prop]);
+                            changed = true;
+                            break;
+                        }
                     }
+                } else {
+                    changed = true;
+                }
+                collectionStates.current = newCollectionStates;
+                if(changed) {
+                    LocalHost.announce().then((announceMessage) => {
+                        console.log('new announce message', announceMessage);
+                        setAnnounceMessage(announceMessage);
+                    });
                 }
             }
         )}, 1000);
         return () => {
             clearInterval(checkInterval);
         }
-    }, [collectionStates]);
-    useEffect(() => {
-        LocalHost.announce().then((announceMessage) => {
-            setAnnounceMessage(announceMessage);
-        });
-    }, [collectionStates]);
+    }, []);
     return announceMessage;
 }
             
